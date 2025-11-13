aa92376a09e403d83fa8834f038fd393339f3d9d
ayowi <auoie_dyncy_tan@dlsl.edu.ph>
2025-11-13 22:56:11 +0800

    Fixed socket URL for deployment

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'wss://final-project-2-appdevt.onrender.com';
 export const socket = io(URL, {
     autoConnect: false
 });
