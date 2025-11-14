794d872dbd3a57a24273d595937091baa19a909d
ayowi <auoie_dyncy_tan@dlsl.edu.ph>
2025-11-14 18:50:03 +0800

    update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'wss://final-project-2-appdevt.onrender.com';
+const URL = 'https://final-project-2-appdevt.onrender.com';
 export const socket = io(URL, {
     autoConnect: false
 });
